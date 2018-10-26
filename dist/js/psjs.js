77303f083e82133fbecadb5913cc8e062a1e6415
Mikhail Sementsov <fewebdev@gmail.com>
2018-10-27 00:01:07 +0300

    `

--- dist/js/psjs.js
+++ dist/js/psjs.js
@@ -137,6 +137,117 @@ function insertElement(tag = "div", value = "", attributes = [], ref = "default"
 }
 
 
-insertElement(`span`, 7, [["id", "qw7"]], sel("body"), "in");
+//log(insertElement(`span`, 7, [["id", "qw7"]], sel("body"), "in"));
 
+function crEl(str, ref = "default", mode = "after") {
+	// get tag
+	let tag = str.match(/\w*[.#( ]?/)[0].match(/\w*/)[0];
 
+	// get value
+	let value = "";
+	if (str.includes("(")) {
+		if (str.indexOf("(") < str.indexOf(" ")) value = str.slice(str.indexOf(")") + 2);
+		else if (str.includes(" ")) value = str.slice(str.indexOf(" ") + 1);
+	}
+	else if (str.includes(" ")) value = str.slice(str.indexOf(" ") + 1);
+
+	// get id + classes + attributes
+	let set = str;
+	if (tag !== "" && value !== "") set = str.slice(str.indexOf(tag) + tag.length, 
+	str.indexOf(value) - 1);
+	else if (tag === "" && value !== "") set = str.slice(0, str.indexOf(" "));
+	set = set.trim();
+
+	// fix tag
+	tag = tag || "div";
+
+	// split id + classes & attributes
+	let [attrs, idcl] = ["", set];
+	if (set.includes("(")) {
+		attrs = set.slice(set.indexOf("(") + 1).replace(")", "");
+		idcl = set.slice(0, set.indexOf("("));
+	}
+
+	// split id & classes
+	let [id, cls] = ["", idcl.split(".")];
+	if (idcl.includes("#")) {
+		id = idcl.slice(idcl.indexOf("#") + 1).split(".")[0];
+		cls = idcl.replace(/#\w*/, "").split(".");
+	}
+	cls.shift();
+
+	// inject classes & id into attributes
+	if (id !== "") attrs += ` id="${id}"`;
+	cls.map(item => {
+		if (attrs.includes("class")) attrs += ` class="${item}"`;
+		else attrs = attrs.replace(`class="`, `class="${item} `);
+	});
+
+	// split attributes
+	attrs = attrs.trim().split(/['"] /);
+	attrs = attrs.map(item => item.replace(/['"]/g, "").split("="));
+
+	// insert element
+	return insertElement(tag, value, attrs, ref, mode);
+}
+
+// let str = [
+// 	`a.q.q2#aa5.oo(href="#" data-num='89') 1. is it good? - yep))`,
+// 	`a.q.q2#aa5.oo(href="#" data-num='89') 1. is it good? - yep))`,
+// 	`a#aa5.oo(href="#" data-num='89') 1. is it good? - yep))`,
+// 	`a(href="#" data-num='89') 1. is it good? - yep))`,
+// 	`a() 1. is it good? - yep))`,
+// 	`a 1. is it good? - yep))`,
+// 	`a`,
+// 	`span 7`,
+// 	`.q 65`
+// ];
+
+// log(str.map(item => crEl(item)));
+
+function inline(str, ref = "default", modex = "after") {
+	// get tag
+	let tag = str.match(/\w*[.#( ]?/)[0].match(/\w*/)[0];
+
+	// get value
+	let value = "";
+	if (str.includes("(")) {
+		if (str.indexOf("(") < str.indexOf(" ")) value = str.slice(str.indexOf(")") + 2);
+		else if (str.includes(" ")) value = str.slice(str.indexOf(" ") + 1);
+	}
+	else if (str.includes(" ")) value = str.slice(str.indexOf(" ") + 1);
+
+	// ???
+	let textEl = null;
+
+	if (value.includes("#[")) {
+		textEl = crEl(str.replace(value, ""), ref, modex);
+
+		let crTN = text => document.createTextNode(text),
+				temp = "",
+				[regex1, regex2] = ["#[", "]"],
+				regex3 = /[\wА-яЁё\t\s ~:;.,!?@$%^&<>(){}_+|'"\-=№*\/\\]*/,
+				regex4 = /#\[[\wА-яЁё\t\s ~:;.,!?@$%^&<>(){}_+|'"\-=№*\/\\]*\]/,
+				mode = true;
+
+		while (value !== "") {
+			let index = value.indexOf(mode ? regex1 : regex2);
+
+			if (index !== -1) {
+				temp = value.slice(0, index).replace(regex1, "");
+				value = value.replace(mode ? regex3 : regex4, "");
+			}
+			else if (value.length) [temp, value] = [value, ""];
+
+			if (mode) textEl.appendChild(crTN(temp));
+			else crEl(temp, textEl, "in");
+
+			mode ^= 1;	// true - number
+		}
+	}
+	else textEl = crEl(str, ref, modex);
+
+	return textEl;
+}
+
+log(inline(`a 1#[span 2]3#[span 4]5`));
