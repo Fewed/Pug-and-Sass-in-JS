cbaa1142ae40809d047f0ee6e62f374f3eb11f81
Mikhail Sementsov <fewebdev@gmail.com>
2019-01-03 00:12:08 +0300

    gitattributes added

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,37 +1,51 @@
-postRender();
+//postRender();
+
+// let pug = `
+
+// div(class="hover")
+// 	ul
+// 		li 1
+// 		/*
+// 		li 2
+// 		li 3
+// 		*/
+// 		li 4
+// 	.g 1#[span 2]3
+// 	// div
+// 	section 1111&nbsp;2222
+
+// `;
+
+// let sass = `
+
+// *
+// 	color: #fff
+// 	background-color: #000
+
+// div
+// 	color: yellow
+// 	border: 1px solid #fff
+// 	&:hover
+// 		cursor: pointer
+// 	& .g
+// 		border: none
+// 		& span
+// 			color: lime
+// 		& div
+// 			color: yellow
+
+// `;
+
+let n = 34;
 
 let pug = `
 
-div(class="hover")
-	ul
-		li 1
-		/*
-		li 2
-		li 3
-		*/
-		li 4
-	.g 1#[span 2]3
-	// div
-	section 1111&nbsp;2222
+textarea(autofocus="")
+button
+	span ${n}
+
 
 `;
 
-let sass = `
 
-*
-	color: #fff
-	background-color: #000
-
-div
-	color: yellow
-	border: 1px solid #fff
-	&:hover
-		cursor: pointer
-	& .g
-		border: none
-		& span
-			color: lime
-		& div
-			color: yellow
-
-`;
+render(pug, "");
